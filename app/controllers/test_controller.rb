81b8af9dcadf7e056d46853e5893805d3c8c9d69
Timo Uhlmann <t.uhlmann@zeroseven.de>
2015-07-28 09:33:37 +0200

    Constructed SO problem http://stackoverflow.com/questions/31613151/request-format-in-rescue-from-block

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,22 @@
 class TestController < ApplicationController
+
+  before_filter only: [:throw] do
+    raise "Some error"
+  end
+
+  rescue_from StandardError, with: :standard_error
+
   def index
   end
+
+  def throw
+  end
+
+  private
+
+  def standard_error error
+    @error = error
+    render 'error', formats: request.format
+  end
+
 end
